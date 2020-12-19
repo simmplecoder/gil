138eceeccf4e88a015ec0a65380b9205d1062f50
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2020-12-19 18:44:25 +0000

    Working hysteresis version
    Still contains artifacts due to
    limitations in the building blocks

--- example/nonmax_suppression.cpp
+++ example/nonmax_suppression.cpp
@@ -1,4 +1,4 @@
-#include "boost/gil/image_view_factory.hpp"
+#include <boost/gil/image_view_factory.hpp>
 #include <algorithm>
 #include <boost/gil.hpp>
 #include <boost/gil/extension/io/png.hpp>
@@ -90,7 +90,7 @@ struct orthogonal_gradient_extractor
         default:
             index_to_point = [](std::ptrdiff_t i, std::ptrdiff_t /*width*/)
             {
-                return gil::point_t(i, 0);
+                return gil::point_t(i, 1);
             };
         }
         for (std::ptrdiff_t i = 0; i < 3; ++i)
@@ -101,12 +101,77 @@ struct orthogonal_gradient_extractor
     }
 };
 
+void hysteresis(const gil::gray16_view_t& input,
+                                     const gil::gray16_view_t& output,
+                                     std::uint16_t soft_threshold,
+                                     std::uint16_t hard_threshold,
+                                     gil::gray16_pixel_t pass_value,
+                                     gil::gray16_pixel_t fail_value) {
+    enum pass_state {
+        fail = 0,
+        soft_pass = 1,
+        pass = 2
+    };
+
+    struct cell {
+        std::uint32_t parent;
+        pass_state state;
+    };
+    std::vector<cell> cells(input.size());
+    for (std::size_t i = 0; i < cells.size(); ++i) {
+        cells[i].parent = i;
+    }
+
+    std::size_t width = static_cast<size_t>(input.width());
+    for (std::size_t y = 0; y < input.height(); ++y) {
+        for (std::size_t x = 0; x < input.width(); ++x) {
+            auto flat_index = y * width + x;
+            auto current_pixel = input(x, y);
+            if (current_pixel < soft_threshold) {
+                cells[flat_index].state = fail;
+                output(x, y) = fail_value;
+                continue;
+            }
+
+            cells[flat_index].state = current_pixel < hard_threshold ? soft_pass : pass;
+
+            auto left_neighbor = flat_index - 1;
+            auto upper_neighbor = flat_index - width;
+            if (x > 0 && cells[flat_index].state != fail) {
+                cells[flat_index].parent = cells[left_neighbor].parent;
+                auto parent = cells[flat_index].parent;
+                auto best_state = std::max(cells[parent].state, cells[flat_index].state);
+                cells[flat_index].state = best_state;
+                cells[parent].state = best_state;
+            } else if (y > 0 && cells[flat_index].state != fail) {
+                cells[flat_index].parent = cells[upper_neighbor].parent;
+                auto parent = cells[flat_index].parent;
+                auto best_state = std::max(cells[parent].state, cells[flat_index].state);
+                cells[flat_index].state = best_state;
+                cells[parent].state = best_state;
+            }
+        }
+    }
+
+    for (std::size_t y = 0; y < input.height(); ++y) {
+        for (std::size_t x = 0; x < input.width(); ++x) {
+            auto flat_index = y * width + x;
+            if (cells[cells[flat_index].parent].state == pass) {
+                output(x, y) = pass_value;
+            } else {
+                output(x, y) = fail_value;
+            }
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
     gil::gray8_image_t input_image;
     gil::read_image(argv[1], input_image, gil::png_tag{});
 
     auto input = gil::view(input_image);
+
     auto sobel_x = gil::generate_dx_sobel();
     auto sobel_y = gil::generate_dy_sobel();
     gil::gray16s_image_t dx_image(input_image.dimensions());
@@ -142,4 +207,17 @@ int main(int argc, char* argv[])
     gil::nonmax_suppression(gradient, 3, gil::gray16_pixel_t(0), suppressed, extractor);
     gil::write_view("suppressed.png", gil::color_converted_view<gil::gray8_pixel_t>(suppressed),
                     gil::png_tag{});
+
+    auto soft_threshold = std::stoul(argv[2]);
+    auto hard_threshold = std::stoul(argv[3]);
+
+    gil::gray16_image_t final_image(input.dimensions());
+    auto final = gil::view(final_image);
+    hysteresis(suppressed,
+               final,
+               static_cast<uint16_t>(soft_threshold),
+               static_cast<uint16_t>(hard_threshold),
+               gil::gray16_pixel_t(std::numeric_limits<std::uint16_t>::max()),
+               gil::gray16_pixel_t(0));
+    gil::write_view("final.png", final, gil::png_tag{});
 }
